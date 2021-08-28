e39e6648b28234280f00a9ddc092e261d7aa691a
yuna1001 <hegapoo1001@gmail.com>
2021-08-28 15:05:14 +0900

    [Mod]テーブル設計

---
+++ database/migrations/2021_08_28_142834_create_tasks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id()->autoIncrement();;
+            $table->string('title')->nullable(false);
+            $table->string('status', 10)->nullable(false);
+            $table->unsignedBigInteger('category_id')->nullable(false);
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropForeign('tasks_category_id_foreign');
+        });
+
+        Schema::dropIfExists('tasks');
+    }
+}
